7f81f0c962169123e8b5b866e58b918e9991de74
brah-ma <1075041845@qq.com>
2017-11-29 16:06:02 +0800

    创建项目，第一次提交

---
+++ app/src/main/java/com/brahma/oschina/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.brahma.oschina;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
